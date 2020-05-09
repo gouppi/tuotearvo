64e992c3fdf3fc22c6b2610d7d23bb6c4380df1d
Janne Nissinen <janne.nissinen@solita.fi>
2020-05-09 23:09:52 +0300

    Rebuilding layout for new review step-by-step

--- src/components/NewReview/ReviewStep0.js
+++ src/components/NewReview/ReviewStep0.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Grid from '@material-ui/core/Grid';
 // import Typography from '@material-ui/core/Typography';
 // import TextField from '@material-ui/core/TextField';
@@ -10,7 +10,9 @@ import {makeStyles} from '@material-ui/core/styles';
 import PhoneIphoneIcon from '@material-ui/icons/PhoneIphone';
 import HeadsetIcon from '@material-ui/icons/Headset';
 import PhotoCameraIcon from '@material-ui/icons/PhotoCamera';
+import ChildFriendlyIcon from '@material-ui/icons/ChildFriendly';
 
+import DesktopWindowsIcon from '@material-ui/icons/DesktopWindows';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -31,35 +33,70 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-export default function ReviewStep0() {
+export default function ReviewStep0(props) {
   const classes = useStyles();
+
+  const [active, setActive] = useState('');
+
+  // Similar to componentDidMount and componentDidUpdate:
+  /*useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `Lisää Arvostelu - Tuotearvostelut.dev`;
+  }, []);
+*/
+
+  const changeButtonActive = () => {
+
+  }
+
   return (
     <React.Fragment>
-
       <Grid container spacing={1} className={classes.gridContainer}>
+       <Grid item>
+          <Button className=""
+          style={{flex:1}}
+          variant="outlined"
+          
+          color="primary"
+          size="large"
+          //onClick={props.nextStep}
+          onClick={() => {setActive('clicked')}}
+          startIcon={<PhoneIphoneIcon style={{ fontSize: 40 }} />}>
+          
+          Puhelimet
+        </Button>
+        </Grid>
+        <Grid item>
           <Button
+          style={{flex:1}}
+          variant="outlined"
+          
+          color="primary"
+          size="large"
+          startIcon={<PhotoCameraIcon style={{ fontSize: 40 }} />}>
+          Kamerat
+        </Button>
+        </Grid>
+        <Grid item>
+          <Button
+          style={{flex:1}}
           variant="outlined"
           color="primary"
           size="large"
-          startIcon={<PhoneIphoneIcon style={{ fontSize: 40 }} />}>
-          Puhelimet
+          startIcon={<DesktopWindowsIcon style={{ fontSize: 40 }} />}>
+          Tietokoneet
         </Button>
+        </Grid>
+        <Grid item>
           <Button
+          style={{flex:1}}
           variant="outlined"
           color="primary"
           size="large"
           startIcon={<HeadsetIcon style={{ fontSize: 40 }} />}>
           Audio
         </Button>
-          <Button
-          variant="outlined"
-          color="primary"
-          size="large"
-          startIcon={<PhotoCameraIcon style={{ fontSize: 40 }} />}>
-          Kamerat
-        </Button>
-        
-       
+        </Grid>
       </Grid>
     </React.Fragment>
   );
--- src/pages/NewReview.js
+++ src/pages/NewReview.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Typography from '@material-ui/core/Typography';
 import {makeStyles} from '@material-ui/core/styles';
 import ReviewStep0 from '../components/NewReview/ReviewStep0';
@@ -8,42 +8,27 @@ import ReviewStep2 from '../components/NewReview/ReviewStep2';
 import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
+import StepContent from '@material-ui/core/StepContent'
 import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        margin: '10px'
-    },
-    layout: {
-        padding: '10px',
-        width: 'auto',
-        marginLeft: theme.spacing(2),
-        marginRight: theme.spacing(2),
-        [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
-          width:'1184px',
-          marginLeft: 'auto',
-          marginRight: 'auto',
-        },
-    },
-    paper: {
-        padding: '20px'
-    },
-    paperTitle: {
-        fontWeight: 200
+ const useStyles = makeStyles((theme) => ({
+    buttons: {
+        paddingTop:'10px'
     }
 }));
 
 const steps = ['Valitse tuotekategoria', 'Anna tuotetiedot', 'Kirjoita arvostelu'];
 
 
-function getStepContent(step) {
+function getStepContent(step, nextStep) {
     switch (step) {
       case 0:
-        return <ReviewStep0/>;
+        return <ReviewStep0 nextStep={nextStep}/>;
       case 1:
-        return <ReviewStep1/>;
+        return <ReviewStep1 nextStep={nextStep}/>;
       case 2:
-        return <ReviewStep2/>;
+        return <ReviewStep2 nextStep={nextStep}/>;
       default:
         throw new Error('Unknown step');
     }
@@ -51,7 +36,9 @@ function getStepContent(step) {
 
 export default function NewReview() {
     const classes = useStyles();
-    const [activeStep, setActiveStep] = React.useState(0);
+    const [activeStep, setActiveStep] = useState(0);
+    const [reviewData, setReviewData] = useState({});
+
 
     const handleNext = () => {
         setActiveStep(activeStep + 1);
@@ -62,34 +49,42 @@ export default function NewReview() {
     };
 
     return (
-        <React.Fragment>
-            <Typography variant="h4" align="center" className="paperTitle">
-                Lisää arvostelu
-            </Typography>
-            <Stepper activeStep={activeStep} className={classes.stepper}>
-                {steps.map((label) => (
+        <Container maxWidth="md">
+            <Typography style={{fontWeight:100}} variant="h5">Uusi arvostelu</Typography>
+            <Stepper activeStep={activeStep} orientation="vertical" className={classes.stepper}>
+                {steps.map((label, i) => (
                 <Step key={label}>
-                    <StepLabel>{label}</StepLabel>
+                    <StepLabel onClick={() => {setActiveStep(i)}}>{label}</StepLabel>
+                    <StepContent>
+                        {getStepContent(activeStep, handleNext)}
+                        <div className={classes.buttons}>
+                        {activeStep !== 0 && (
+                            <Button onClick={handleBack}
+                                    color="secondary"
+                                    variant="outlined" 
+                                    className={classes.button}>
+                                Takaisin
+                            </Button>
+                        )}
+                            <Button
+                                variant="outlined"
+                                color="primary"
+                                onClick={handleNext}
+                                className={classes.button}>
+                                {activeStep === steps.length - 1 ? 'Place order' : 'Seuraava'}
+                            </Button>
+                        </div>
+                    </StepContent>
                 </Step>
+                /* <Grid item md={6}>
+                
+                </Grid> */
+                
                 ))}
             </Stepper>
-                {getStepContent(activeStep)}
-                <div className={classes.buttons}>
-                    {activeStep !== 0 && (
-                        <Button onClick={handleBack} className={classes.button}>
-                        Back
-                        </Button>
-                    )}
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        onClick={handleNext}
-                        className={classes.button}
-                    >
-                        {activeStep === steps.length - 1 ? 'Place order' : 'Next'}
-                    </Button>
-                </div>
-        </React.Fragment>
+            
+                
+        </Container>
                     
     );
 }
